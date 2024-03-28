03a264faea0cc8ba99775fcd746a3a3b8681e421
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-03-28 16:08:13 +0100

    fix: comment offers page

--- frontend/app/offer/page.tsx
+++ frontend/app/offer/page.tsx
@@ -1,19 +1,20 @@
-import db from '@wa/prisma';
+// import db from '@wa/prisma';
 
 export default async function Offer() {
-  const offers = await db.jobOffer.findMany({
-    orderBy: { created_at: 'desc' },
-  });
+  // const offers = await db.jobOffer.findMany({
+  //   orderBy: { created_at: 'desc' },
+  // });
+
   return (
     <div>
-      <h1>Job Offers</h1>
-      <ul>
+      <h1>Offer</h1>
+      {/* <ul>
         {offers.map((offer) => (
           <li key={offer.id}>
             {offer.title} - {offer.description}
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
