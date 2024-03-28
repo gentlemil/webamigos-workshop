3749592dc9e2af29c0ef4b635529c9ea88bd1c61
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-03-28 14:10:55 +0100

    feat: prisma setup

---
+++ prisma/src/lib/migrations/20240328125506_initial/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "JobOffer" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "company" TEXT,
+    "location" TEXT,
+    "salary_from" INTEGER NOT NULL,
+    "salary_to" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "JobOffer_pkey" PRIMARY KEY ("id")
+);
