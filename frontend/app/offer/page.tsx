423f8dcabb935b1f9a9d51dcfe239241d9f0c903
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-03-28 16:36:08 +0100

    feat: offers page

--- frontend/app/offer/page.tsx
+++ frontend/app/offer/page.tsx
@@ -1,20 +1,20 @@
-// import db from '@wa/prisma';
+import db from '@wa/prisma';
 
 export default async function Offer() {
-  // const offers = await db.jobOffer.findMany({
-  //   orderBy: { created_at: 'desc' },
-  // });
+  const offers = await db.jobOffer.findMany({
+    orderBy: { created_at: 'desc' },
+  });
 
   return (
     <div>
       <h1>Offer</h1>
-      {/* <ul>
+      <ul>
         {offers.map((offer) => (
           <li key={offer.id}>
             {offer.title} - {offer.description}
           </li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   );
 }
